0ec555e6ec386375e2c9b962c0c47777a1aa620e
박태연 <158217246+TP228@users.noreply.github.com>
2025-06-28 14:05:07 +0900

    update the test code

--- ty_test/plane_strain_mpm.py
+++ ty_test/plane_strain_mpm.py
@@ -1,6 +1,6 @@
 from geotaichi import *
 
-init(dim=3, device_memory_GB=4)
+init(dim=3, device_memory_GB=8)
 
 mpm = MPM()
 
@@ -16,12 +16,12 @@ mpm.set_solver({
                     "Timestep":         1e-6,
                     "SimulationTime":   0.2,
                     "SaveInterval":     0.002,
-                    "SavePath":         './ty_test/DiskTarget_3D'
+                    "SavePath":         './ty_test/DiskTarget'
                })
 
 mpm.memory_allocate(memory={
-                            "max_material_number":           2,
-                            "max_particle_number":           300000,
+                            "max_material_number":           1,
+                            "max_particle_number":           1000000,
                             "max_constraint_number":  {
                                                            "max_reflection_constraint":   80000,
                                                            "max_velocity_constraint":   25000
@@ -32,10 +32,17 @@ mpm.memory_allocate(memory={
 mpm.add_contact(contact_type="MPMContact", friction=0.28)
 
 mpm.add_material(model="DruckerPrager",
-                 material={ "MaterialID": 1, "Density": 600, "YoungModulus": 6.1e7, "PoissionRatio": 0.2, "Friction": 16, "Dilation": 3.0 })
-
-mpm.add_material(model="LinearElastic",
-                 material={ "MaterialID": 2, "Density": 7850, "YoungModulus": 1e10, "PoissionRatio": 0.3 })
+                 material={ 
+                     "MaterialID": 1, 
+                     "Density": 600, 
+                     "YoungModulus": 6.1e7,
+                    "PoissionRatio": 0.2, 
+                    "Friction": 16, 
+                    "Dilation": 3.0,
+                    "Cohesion": 0.0,
+                    "Tensile": 0.0 
+                    }
+                    )
 
 mpm.add_element(element={ "ElementType": "R8N3D", "ElementSize": [0.01, 0.01, 0.01] })
 
@@ -61,6 +68,10 @@ mpm.add_body(body={
                                    "nParticlesPerCell":  2,
                                    "BodyID":             0,
                                    "MaterialID":         1,
+                                    "ParticleStress": {
+                                                            "GravityField":     True,
+                                                            "InternalStress":   ti.Vector([-0., -0., -0., 0., 0., 0.])
+                                                        },
                                    "InitialVelocity":    ti.Vector([0, 0, 0]),
                                    "FixVelocity":        ["Free", "Free", "Free"]
                                },
@@ -68,7 +79,8 @@ mpm.add_body(body={
                                    "RegionName":         "sphere_region",
                                    "nParticlesPerCell":  2,
                                    "BodyID":             1,
-                                   "MaterialID":         2,
+                                   "RigidBody":          True,
+                                   "Density":            7850,
                                    "InitialVelocity":    ti.Vector([0., 0., -1.12]),
                                    "FixVelocity":        ["Free", "Free", "Free"]
                                }]
@@ -80,11 +92,34 @@ mpm.add_boundary_condition(boundary=[{
                                         "StartPoint":     [0, 0, 0],
                                         "EndPoint":       [1.02, 0.12, 0.02]
                                     },
-                                    { "BoundaryType": "ReflectionConstraint", "Norm": [-1, 0., 0], "StartPoint": [0., 0., 0.], "EndPoint": [0.02, 0.14, 0.5] },
-                                    { "BoundaryType": "ReflectionConstraint", "Norm": [1, 0., 0], "StartPoint": [1.02, 0, 0], "EndPoint": [1.04, 0.14, 0.5] },
-                                    { "BoundaryType": "ReflectionConstraint", "Norm": [0, -1., 0], "StartPoint": [0., 0., 0.], "EndPoint": [1.04, 0.02, 0.5] },
-                                    { "BoundaryType": "ReflectionConstraint", "Norm": [0, 1., 0], "StartPoint": [0, 0.12, 0], "EndPoint": [1.04, 0.14, 0.5] }])
-
+                                    
+                                    {
+                                        "BoundaryType":   "ReflectionConstraint",
+                                        "Norm":           [-1, 0., 0],
+                                        "StartPoint":     [0., 0., 0.],
+                                        "EndPoint":       [0.02, 0.14, 0.5]
+                                    },
+                                    
+                                    {
+                                        "BoundaryType":   "ReflectionConstraint",
+                                        "Norm":           [1, 0., 0],
+                                        "StartPoint":     [1.02, 0, 0],
+                                        "EndPoint":       [1.04, 0.14, 0.5]
+                                    },
+                                    
+                                    {
+                                        "BoundaryType":   "ReflectionConstraint",
+                                        "Norm":           [0, -1., 0],
+                                        "StartPoint":     [0., 0., 0.],
+                                        "EndPoint":       [1.04, 0.02, 0.5]
+                                    },
+                                    
+                                    {
+                                        "BoundaryType":   "ReflectionConstraint",
+                                        "Norm":           [0, 1., 0],
+                                        "StartPoint":     [0, 0.12, 0],
+                                        "EndPoint":       [1.04, 0.14, 0.5]
+                                    }])
 mpm.select_save_data()
 mpm.run()
 mpm.postprocessing()
--- ty_test/sphere_impact_mpm.py
+++ ty_test/sphere_impact_mpm.py
@@ -1,34 +1,32 @@
 from geotaichi import *
 
-init(arch='cpu')
+init(dim=3, arch='cpu')
 
 mpm = MPM()
 
-mpm.set_configuration(domain=ti.Vector([10., 6., 5.]),
+mpm.set_configuration(domain=[10., 6., 5.],
                       background_damping=0., 
                       alphaPIC=0.005, 
                       mapping="USF", 
-                      shape_function="Linear",
-                      gravity=ti.Vector([0., 0., 0.]),
-                      velocity_projection="Affine")
+                      shape_function="GIMP",
+                      gravity=[0., 0., 0.])
 
 mpm.set_solver({
-                      "Timestep":         1e-6,
+                      "Timestep":         1e-5,
                       "SimulationTime":   0.1,
                       "SaveInterval":     1e-3,
-                      "SavePath":         './DiskDisk'
-                 })    
-     
+                      "SavePath":         './ty_test/DiskDisk'
+                 })
+
 mpm.memory_allocate(memory={
                                 "max_material_number":           1,
-                                "max_particle_number":           56000,
-                                "verlet_distance_multiplier":    1.,
+                                "max_particle_number":           80000,
                                 "max_constraint_number":  {
-                                                               "max_reflection_constraint":   541681
+                                                               "max_reflection_constraint":   61820
                                                           }
                             })
 
-mpm.add_contact(contact_type="MPMContact", friction=0.)
+mpm.add_contact(contact_type="MPMContact", friction=0.5)
 
 mpm.add_material(model="LinearElastic",
                  material={
@@ -40,55 +38,41 @@ mpm.add_material(model="LinearElastic",
 
 mpm.add_element(element={
                              "ElementType":               "R8N3D",
-                             "ElementSize":               ti.Vector([0.1, 0.1, 0.1]),
-                             "Contact":                   {
-                                                               "ContactDetection":                True
-                                                          } 
+                             "ElementSize":               [0.1, 0.1, 0.1]
                         })
 
+sphere1_center_py = ti.Vector([4.4, 3.0, 3.0])
+sphere1_radius_py = 0.5
+sphere2_center_py = ti.Vector([5.6, 3.0, 3.0])
+sphere2_radius_py = 0.5
+
 mpm.add_region(region=[{
-                            "Name": "region1",
+                            "Name": "sphere1_region",
                             "Type": "Spheroid",
-                            "BoundingBoxPoint": ti.Vector([5.1, 2.5, 2.5]),
-                            "BoundingBoxSize": ti.Vector([0.25, 0.25, 0.25]),
-                            "zdirection": ti.Vector([0., 0., 1.])
+                            "BoundingBoxPoint": sphere1_center_py - sphere1_radius_py,
+                            "BoundingBoxSize": [2 * sphere1_radius_py, 2 * sphere1_radius_py, 2 * sphere1_radius_py]
                       },
-                      
                       {
-                            "Name": "region2",
+                            "Name": "sphere2_region",
                             "Type": "Spheroid",
-                            "BoundingBoxPoint": ti.Vector([4.4, 3.0, 3.0]),
-                            "BoundingBoxSize": ti.Vector([0.25, 0.25, 0.25]),
-                            "zdirection": ti.Vector([0., 0., 1.])
+                            "BoundingBoxPoint": sphere2_center_py - sphere2_radius_py,
+                            "BoundingBoxSize": [2 * sphere2_radius_py, 2 * sphere2_radius_py, 2 * sphere2_radius_py]
                       }])
 
 mpm.add_body(body={
                        "Template": [{
-                                       "RegionName":         "region1",
+                                       "RegionName":         "sphere1_region",
                                        "nParticlesPerCell":  4,
                                        "BodyID":             0,
                                        "MaterialID":         1,
-                                       "ParticleStress": {
-                                                              "GravityField":     False,
-                                                              "InternalStress":   ti.Vector([-0., -0., -0., 0., 0., 0.])
-                                                         },
-                                       "InitialVelocity":ti.Vector([-1, 0, 0]),
-                                       "FixVelocity":    ["Free", "Free", "Free"]    
-                                       
+                                       "InitialVelocity":    ti.Vector([2., 0., 0.])
                                    },
-                                   
                                    {
-                                       "RegionName":         "region2",
+                                       "RegionName":         "sphere2_region",
                                        "nParticlesPerCell":  4,
                                        "BodyID":             1,
                                        "MaterialID":         1,
-                                       "ParticleStress": {
-                                                              "GravityField":     False,
-                                                              "InternalStress":   ti.Vector([-0., -0., -0., 0., 0., 0.])
-                                                         },
-                                       "InitialVelocity":ti.Vector([1, 0, 0]),
-                                       "FixVelocity":    ["Free", "Free", "Free"]    
-                                       
+                                       "InitialVelocity":    ti.Vector([-2., 0., 0.])
                                    }]
                    })
 
@@ -98,37 +82,11 @@ mpm.add_boundary_condition(boundary=[{
                                         "StartPoint":     [0., 0., 0.],
                                         "EndPoint":       [10., 6., 0.]
                                     },
-                                    
-                                    {
-                                        "BoundaryType":   "ReflectionConstraint",
-                                        "Norm":           [-1, 0., 0],
-                                        "StartPoint":     [0., 0., 0.],
-                                        "EndPoint":       [0., 2., 0.]
-                                    },
-                                    
-                                    {
-                                        "BoundaryType":   "ReflectionConstraint",
-                                        "Norm":           [1, 0., 0],
-                                        "StartPoint":     [6., 0., 0.],
-                                        "EndPoint":       [6., 2., 0.]
-                                    },
-                                    
-                                    {
-                                        "BoundaryType":   "ReflectionConstraint",
-                                        "Norm":           [0, -1., 0],
-                                        "StartPoint":     [0., 0., 0.],
-                                        "EndPoint":       [6., 0., 0.]
-                                    },
-                                    
-                                    {
-                                        "BoundaryType":   "ReflectionConstraint",
-                                        "Norm":           [0, 1., 0],
-                                        "StartPoint":     [0., 2., 0.],
-                                        "EndPoint":       [6., 2., 0.]
-                                    }])
+                                    { "BoundaryType": "ReflectionConstraint", "Norm": [-1, 0., 0], "StartPoint": [0., 0., 0.], "EndPoint": [0., 6., 5.] },
+                                    { "BoundaryType": "ReflectionConstraint", "Norm": [1, 0., 0], "StartPoint": [10., 0., 0.], "EndPoint": [10., 6., 5.] },
+                                    { "BoundaryType": "ReflectionConstraint", "Norm": [0, -1., 0], "StartPoint": [0., 0., 0.], "EndPoint": [10., 0., 5.] },
+                                    { "BoundaryType": "ReflectionConstraint", "Norm": [0, 1., 0], "StartPoint": [0., 6., 0.], "EndPoint": [10., 6., 5.] }])
 
-mpm.select_save_data(grid=True)
-
+mpm.select_save_data()
 mpm.run()
-
 mpm.postprocessing()
